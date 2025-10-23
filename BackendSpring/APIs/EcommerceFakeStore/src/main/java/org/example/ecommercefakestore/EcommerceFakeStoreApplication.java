7d504136fd4c8bc23ade23f9dc4ec8ab7c74b344
venkatasai kadamati <venkatasaikadamati@gmail.com>
2025-10-23 19:29:18 +0530

    initial commit

---
+++ BackendSpring/APIs/EcommerceFakeStore/src/main/java/org/example/ecommercefakestore/EcommerceFakeStoreApplication.java
@@ -0,0 +1,13 @@
+package org.example.ecommercefakestore;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EcommerceFakeStoreApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EcommerceFakeStoreApplication.class, args);
+	}
+
+}
---
+++ DSA/Topicwise/Arrays/Basics/BasicProblems.java
@@ -0,0 +1,4 @@
+package DSA.Topicwise.Arrays.Basics;
+
+public class BasicProblems {
+}
