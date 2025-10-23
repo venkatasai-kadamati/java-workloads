f1bbaed80f3f44db81b9ca7e8a2c4bb0a0bf3c0e
venkatasai kadamati <venkatasaikadamati@gmail.com>
2025-10-23 21:17:19 +0530

    Added dotenv setup for basic .env setup

--- BackendSpring/APIs/EcommerceFakeStore/src/main/java/org/example/ecommercefakestore/EcommerceFakeStoreApplication.java
+++ BackendSpring/APIs/EcommerceFakeStore/src/main/java/org/example/ecommercefakestore/EcommerceFakeStoreApplication.java
@@ -1,5 +1,6 @@
 package org.example.ecommercefakestore;
 
+import io.github.cdimascio.dotenv.Dotenv;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
@@ -7,7 +8,15 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class EcommerceFakeStoreApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(EcommerceFakeStoreApplication.class, args);
+		// if we want to load something, we should do it before the springboot server
+        // load env variables from .env file
+        Dotenv dotenv = Dotenv.configure().load();
+
+        // set system properties: simply take those dotenv defined variable into os.
+        dotenv.entries().forEach(entry -> {
+            System.setProperty(entry.getKey(), entry.getValue());
+        });
+        SpringApplication.run(EcommerceFakeStoreApplication.class, args);
 	}
 
 }
